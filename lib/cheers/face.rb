c0a63889dc1b7e7afba8427c221677a65cbdc86f
Laura Williams <lauraannwilliams@gmail.com>
2018-05-12 22:51:21 -0700

    switch to use minimagick

--- lib/cheers/face.rb
+++ lib/cheers/face.rb
@@ -2,9 +2,14 @@ module Cheers
   class Face < ImageComponent
     
     def apply
+      puts "Applying #{self.class.name}"
+
+      wip = canvas
       [UpperGlow, LowerGlow, Texture, Mouth].each do |klass|
-        klass.new(canvas, color_randomizer, image_randomizer.dup).apply
+        wip = klass.new(wip, color_randomizer, image_randomizer.dup).apply
       end
+
+      wip
     end
     
   end
